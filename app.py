e98fbc76c106763389a7341a9d82cbcf664f41f3
Luccas Magalhães <luccas.magalhaes@hotmail.com>
2025-02-04 05:09:48 -0300

    Adicionado answers.py e respondido perguntas 9,13,14

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from src.extraction import load_data
 st.set_page_config(layout='wide')
 
 def main():
-    df=load_data()
-    st.dataframe(df)
+    df_raw=load_data()
+    st.dataframe(df_raw)
 
 if __name__=='__main__':
     main()
